55cd63d264eec57868413367268fdb89624f5fc8
ahmadtaheri1991 <ahmadtaheri1991@gmail.com>
2024-01-17 07:37:40 +0330

    ahmad

---
+++ app/app.vue
@@ -0,0 +1,28 @@
+<script>
+import { defineNuxtComponent } from "nuxt/app";
+
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: [],
+  }),
+  methods: {
+    fetchTodoList() {
+      fetch("https://jsonplaceholder.typicode.com/todos/")
+        .then((response) => response.json())
+        .then((json) => {
+          this.todoList = json;
+        });
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>Hello Frontend Masters</h1>
+    <button @click="fetchTodoList">Fetch Data</button>
+    <pre>
+      {{ todoList }}
+    </pre>
+  </div>
+</template>
