826f52be945b4d373c3ad0201f7519cff493aad9
ahmadtaheri1991 <ahmadtaheri1991@gmail.com>
2024-01-18 16:06:24 +0330

    feature: add todo list viewer example

--- app/app.vue
+++ app/app.vue
@@ -14,15 +14,33 @@ export default defineNuxtComponent({
         });
     },
   },
+  
 });
 </script>
 
 <template>
   <div>
+    <img src="/todo.jpg" alt="Todo photo by Glenn Carstens-Peters" />
+    <p>
+      Photo by
+      <a
+        href="https://unsplash.co/@glenncarstenspeters?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+      >
+        Glenn Carstens-Peters
+      </a>
+      on
+      <a
+        href="https://unsplash.com/photos/person-writing-bucket-list-on-book-RLw-UC03Gwc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+      >
+        Unsplash
+      </a>
+    </p>
     <h1>Hello Frontend Masters</h1>
     <button @click="fetchTodoList">Fetch Data</button>
-    <pre>
-      {{ todoList }}
-    </pre>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed" /> {{ todo.title }}
+      </li>
+    </ul>
   </div>
 </template>
